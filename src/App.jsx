cbb0f647b4a2af1b62fcb89bffd6bcfafb55a704
Muhammad <muhammadarbobzoda@gmail.com>
2024-11-24 18:27:04 +0300

    second chenge

--- src/App.jsx
+++ src/App.jsx
@@ -1,81 +1,76 @@
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';  
-import './App.css';  
-import Layout from './layout/layout';  
-import First from './pages/first/first';  
-import Second from './pages/second/second';  
-import Third from './pages/third/third';  
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import './App.css';
+import Layout from './layout/layout';
+import First from './pages/first/first';
+import Second from './pages/second/second';
+import Third from './pages/third/third';
 import Fourth from './pages/fourth/fourth'; // Ensure this import path matches the filesystem  
-import Fifth from './pages/fifth/fifth';  
-import Sixth from './pages/sixth/sixth';  
-import Seventh from './pages/seventh/seventh';  
-import Eighth from './pages/eighth/eighth';  
-import Ninth from './pages/ninth/ninth';  
-import Tenth from './pages/tenth/tenth';  
-import Eleventh from './pages/eleventh/eleventh';  
+import Sixth from './pages/sixth/sixth';
+import Seventh from './pages/seventh/seventh';
+import Eighth from './pages/eighth/eighth';
+import Ninth from './pages/ninth/ninth';
+import Tenth from './pages/tenth/tenth';
+import Eleventh from './pages/eleventh/eleventh';
 import Twelfth from './pages/twelfth/twelfth'; // Ensure the import path is correct  
-import Thirteenth from './pages/thirteenth/thirteenth';  
+import Thirteenth from './pages/thirteenth/thirteenth';
 
-const router = createBrowserRouter([  
-  {  
-    path: "/",  
-    element: <Layout />,  
-    children: [  
-      {  
-        index: true,  
-        element: <First />  
-      },  
-      {  
-        path: "/second",  
-        element: <Second />  
-      },  
-      {  
-        path: "/third",  
-        element: <Third />  
-      },  
-      {  
-        path: "/fourth",   
-        element: <Fourth />  
-      },  
-      {  
-        path: "/fifth",  
-        element: <Fifth />  
-      },  
-      {  
-        path: "/sixth",  
-        element: <Sixth />  
-      },  
-      {  
-        path: "/seventh",  
-        element: <Seventh />  
-      },  
-      {  
-        path: "/eighth",  
-        element: <Eighth />  
-      },  
-      {  
-        path: "/ninth",  
-        element: <Ninth />  
-      },  
-      {  
-        path: "/tenth",  
-        element: <Tenth />  
-      },  
-      {  
-        path: "/eleventh",  
-        element: <Eleventh />  
-      },  
-      {  
-        path: "/twelfth",   
-        element: <Twelfth />  
-      },  
-      {  
-        path: "/thirteenth", 
-        element: <Thirteenth />  
-      },  
-    ],  
-  }  
-]);  
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      {
+        index: true,
+        element: <First />
+      },
+      {
+        path: "/second",
+        element: <Second />
+      },
+      {
+        path: "/third",
+        element: <Third />
+      },
+      {
+        path: "/fourth",
+        element: <Fourth />
+      },
+      {
+        path: "/sixth",
+        element: <Sixth />
+      },
+      {
+        path: "/seventh",
+        element: <Seventh />
+      },
+      {
+        path: "/eighth",
+        element: <Eighth />
+      },
+      {
+        path: "/ninth",
+        element: <Ninth />
+      },
+      {
+        path: "/tenth",
+        element: <Tenth />
+      },
+      {
+        path: "/eleventh",
+        element: <Eleventh />
+      },
+      {
+        path: "/twelfth",
+        element: <Twelfth />
+      },
+      {
+        path: "/thirteenth",
+        element: <Thirteenth />
+      },
+    ],
+  }
+]);
 
-export default function App() {  
-  return <RouterProvider router={router} />;  
+export default function App() {
+  return <RouterProvider router={router} />;
 }
--- src/pages/fourth/fourth.jsx
+++ src/pages/fourth/fourth.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import icon9 from './images/icon (9).png'
 import img41 from './images/Image (41).png'
 import el28 from './images/Ellipse 28.png'
@@ -13,50 +13,72 @@ import rec284 from './images/Rectangle 283 (1).png'
 import('./fourth.css')
 
 export function ProDeteil() {
-  return <div className='box1'>
-    <div className='header-box1'>
-      <img src={icon9} alt="" />
-      <b>Product Detail</b>
-    </div>
+  const [modal, setModal] = useState(false);
 
-    <div className='div-in-box1'>
-      <img className='img-1' src={img41} alt="" />
-      <div className='div-2'>
-        <h2>Project Sun-Glass</h2>
-        <p>A collection of 10,000 utility-enabled PFPs that feature a richly diverse and unique pool of rarity-powered traits. </p>
-        <div className='div-users'>
-          <div className='user'>
-            <img src={el28} alt="" />
-            <div >
-              <p>Created by</p>
-              <b>Perperzon</b>
-            </div>
-          </div>
-          <div className='user' >
-            <img src={el29} alt="" />
-            <div>
-              <p>Owned by</p>
-              <b>Videz</b>
-            </div>
-          </div>
-        </div>
+  const closeModal = () => {
+    setModal(false);
+  };
 
-
-        <div className='div3-in-box1'>
-          <div>
-            <p>Current Bid</p>
-            <div style={{ display: 'flex', alignItems: 'center' }}><img src={eth} alt="" /><h3>1.75</h3></div>
-          </div>
-          <div className='right-div'>
-            <p>End in</p>
-            <b>Jun 17, 2023 at 05:08</b>
-          </div>
-        </div>
-        <button className='button1'><img src={vec} alt="" />Place Bid</button>
+  return (
+    <div className="box1">
+      <div className="header-box1">
+        <img src={icon9} alt="Icon" />
+        <b>Product Detail</b>
       </div>
+
+      <div className="div-in-box1">
+        <img className="img-1" src={img41} alt="Project Sun-Glass" />
+        <div className="div-2">
+          <h2>Project Sun-Glass</h2>
+          <p>
+            A collection of 10,000 utility-enabled PFPs that feature a richly diverse and unique pool of rarity-powered traits.
+          </p>
+          <div className="div-users">
+            <div className="user">
+              <img src={el28} alt="Created by Perperzon" />
+              <div>
+                <p>Created by</p>
+                <b>Perperzon</b>
+              </div>
+            </div>
+            <div className="user">
+              <img src={el29} alt="Owned by Videz" />
+              <div>
+                <p>Owned by</p>
+                <b>Videz</b>
+              </div>
+            </div>
+          </div>
+
+          <div className="div3-in-box1">
+            <div>
+              <p>Current Bid</p>
+              <div style={{ display: 'flex', alignItems: 'center' }}>
+                <img src={eth} alt="Ethereum" />
+                <h3>1.75</h3>
+              </div>
+            </div>
+            <div className="right-div">
+              <p>End in</p>
+              <b>Jun 17, 2023 at 05:08</b>
+            </div>
+          </div>
+          <button onClick={() => setModal(true)} className="button1">
+            <img src={vec} alt="Place bid icon" />
+            Place Bid
+          </button>
+        </div>
+      </div>
+
+      {modal && (
+        <div className="modal">
+          
+        </div>
+      )}
     </div>
-  </div>
+  );
 }
+
 
 
 export function FromCreator() {
